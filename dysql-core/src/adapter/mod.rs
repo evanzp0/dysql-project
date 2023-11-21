e3ccf548a0d618912b94a3d0a6b6660c4f0cb3a3
evan <564374058@qq.com>
2023-11-21 14:12:27 +0800

    add tokio-postgres deps

--- dysql-core/src/adapter/mod.rs
+++ dysql-core/src/adapter/mod.rs
@@ -3,3 +3,10 @@ mod sqlx_adapter;
 
 #[cfg(feature = "sqlx")]
 pub use sqlx_adapter::*;
+
+
+#[cfg(feature = "tokio-postgres")]
+mod tokio_pg_adapter;
+
+#[cfg(feature = "tokio-postgres")]
+pub use tokio_pg_adapter::*;

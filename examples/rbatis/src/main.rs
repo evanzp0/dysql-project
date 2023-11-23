354750cc0003d4f4e96af6bb3044851eb5684fd4
evan <564374058@qq.com>
2023-11-23 21:06:49 +0800

    add rbatis example

---
+++ examples/rbatis/src/main.rs
@@ -0,0 +1,45 @@
+use rbatis::RBatis;
+use rbdc_pg::PgDriver;
+use serde::{Serialize, Deserialize};
+
+#[tokio::main]
+async fn main() {
+    let rb = RBatis::new();
+    rb.init(PgDriver{},"postgres://root:111111@localhost:5432/my_database").unwrap();
+    let dto = UserDto{ id: Some(2), name: Some("ab".to_owned()), age: Some(13) , id_rng: None };
+    let sql = "select * from test_user where name = $1";
+
+    let rb_args = vec![rbs::to_value!(&dto.name)];
+
+    // let rst: Option<User> = rb
+    //     .query_decode(sql, rb_args.clone())
+    //     .await
+    //     .unwrap();
+
+    let r = rb.query(&sql, rb_args).await.unwrap();
+    let rst = rbatis::decode::decode::<Option<User>>(r);
+
+    println!("{:?}", rst)
+}
+
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct UserDto {
+    pub id: Option<i64>,
+    pub name: Option<String>,
+    pub age: Option<i32>,
+    pub id_rng: Option<Vec<i32>>,
+}
+
+impl UserDto {
+    pub fn new(id: Option<i64>, name: Option<String>, age: Option<i32>, id_rng: Option<Vec<i32>>) -> Self {
+        Self { id, name, age, id_rng }
+    }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct User {
+    pub id: i64,
+    pub name: Option<String>,
+    pub age: Option<i32>,
+}

aeb0312bbdbd058d0c004c42a09e5359fe5e209b
evan <564374058@qq.com>
2024-04-05 22:41:35 +0800

    update

--- tests/test_sqlx/common.rs
+++ tests/test_sqlx/common.rs
@@ -13,7 +13,7 @@ pub struct UserDto {
 
 #[allow(dead_code)]
 impl UserDto {
-    pub fn new(id: Option<i64>, name: Option<String>, age: Option<u8>, id_rng: Option<Vec<i32>>) -> Self {
+    pub fn new(id: Option<i64>, name: Option<String>, age: Option<i32>, id_rng: Option<Vec<i32>>) -> Self {
         Self { id, name, age, id_rng }
     }
 }
